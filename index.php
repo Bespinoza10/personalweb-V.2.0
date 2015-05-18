c7756c1170abbbfba9586a0c56875bf81517e2f2
Brandon Espinoza <espinozab100@gmail.com>
2015-05-18 11:25:58 -0500

    Modified foundation css and slick slider css. Finished Index.php. Site is responsive

--- index.php
+++ index.php
@@ -20,94 +20,170 @@
             <a class="links" href="#about">About Me</a>
           </li>
           <li class="item">
-            <a href="#contact" class="contact">Contact</a>
+            <a href="#offer" class="links">What I Offer</a>
           </li>
+          <li class="item">
+            <a href="#portfolio" class="links">My Portfolio</a>
+          </li>
+          <li class="item">
+            <a href="#testimonials" class="links">Testimonials</a>
+          </li>
+          <li class="item">
+            <a href="#cont" class="contact">Contact</a>
+          </li>
+          <button data-dropdown="drop" aria-controls="drop" aria-expanded="false" class="small elMenu dropdown"><img src="img/menuButtonR.png"></button><br>
+            <ul id="drop" data-dropdown-content class="f-dropdown" role="menu" aria-hidden="false" tabindex="-1">
+              <li><a href="#">Home</a></li>
+              <li><a href="#about">About Me</a></li>
+              <li><a href="#offer">What I Offer</a></li>
+              <li><a href="#portfolio">My Portfolio</a></li>
+              <li><a href="#testimonials">Testimonials</a></li>
+              <li><a href="#cont">Contact</a></li>
+            </ul>
         </ul>
 
         <div class="logo">
-          <a href="#"><h1 class="brandN">Brandon Espinoza</h1></a>
+          <a href="#"><h1 class=" brandN">Brandon Espinoza</h1></a>
         </div>
       </div>
     </header>
 
 <!--     SLIDER -->
-    <div class="large-12 columns sliderMain"></div>
+    <div class="large-12 columns sliderMain">
+      <div class="small-11 small-centered medium-6 medium-uncentered medium-offset-6 large-6 large-offset-6 columns txtmain">
+        <h1 class="maintxt">Welcome to my Website!</h1><hr>
+        <p class="bioHs1">I make awesome websites and apps</p>
+      </div>
+    </div>
 <!--     ABOUT SECTION -->
     <div id="about" class="large-12 columns">
-      <div class="large-4 large-centered columns">
+      <div class="small-8 small-centered medium-8 medium-centered large-4 large-centered columns">
         <h2 class="name">Brandon Espinoza </h2>
       </div>
-      <div class="large-4 large-centered columns">
+      <div class="small-9 small-centered medium-8 medium-centered large-4 large-centered columns">
         <h4 class="name2">Web Designer/Developer, Photographer, Graphic Designer </h4>
       </div>
 
-      <div class="large-2 large-centered columns"><hr class="hrs"></div>
+      <div class=" small-6 small-centered large-2 large-centered columns"><hr class="medium-11 columns hrs"></div>
 
       <div class="large-6 columns">
-        <div class="large-2 large-offset-1 columns">
+        <div class="large-2 medium-6 medium-offset-1 large-offset-1 columns">
           <h4 class="bioHs">Profile:</h4>
         </div>
         <div class="large-7 columns">
-          <h4 class="bioHs1">Web Designer/Developer, Photographer, Graphic Designer</h4>
+          <h4 class="bioHs1 small-10 small-offset-1">Web Designer/Developer, Photographer, Graphic Designer</h4>
         </div>
-        <div class="large-2 large-offset-1 columns">
+        <div class="large-2 medium-6 medium-offset-1 large-offset-1 columns">
           <h4 class="bioHs">Skills in:</h4>
         </div>
             <!---PROGRESS BARS--->
-        <div class="large-7 columns">
+        <div class="small-10 small-offset-1 large-7 columns">
           <div class="progress large-12 secondary progBar round">
-            <span class="meter progBar1"><span class="large-1 large-offset-1 columns bioHs2">HTML</span><span class="large-1 large-offset-9 columns bioHs2">90%</span></span>
+            <span class="meter progBar1"><span class=" small-3 large-1 large-offset-1 columns bioHs2">HTML</span><span class="small-1 small-offset-7 large-1 large-offset-9 columns bioHs2">90%</span></span>
           </div>
           <div class="progress large-12 secondary progBar round">
-            <span class="meter progBar1"><span class="large-1 large-offset-1 columns bioHs2">CSS</span><span class="large-1 large-offset-9 columns bioHs2">90%</span></span>
+            <span class="meter progBar1"><span class="small-3 large-1 large-offset-1 columns bioHs2">CSS</span><span class="small-1 small-offset-7 large-1 large-offset-9 columns bioHs2">90%</span></span>
           </div>
           <div class="progress large-12 secondary progBar round">
-            <span class="meter progBar3"><span class="large-1 large-offset-1 columns bioHs2">JavaScript</span><span class="large-1 large-offset-9 bioHs2 columns">80%</span></span>
+            <span class="meter progBar3"><span class="small-3 large-1 large-offset-1 columns bioHs2">JavaScript</span><span class="small-1 small-offset-7 large-1 large-offset-9 bioHs2 columns">80%</span></span>
           </div>
           <div class="progress large-12 secondary progBar round">
-            <span class="meter progBar3"><span class="large-1 large-offset-1 columns bioHs2">Photoshop</span><span class="large-1 large-offset-9 bioHs2 columns">80%</span></span>
+            <span class="meter progBar3"><span class="small-1 large-1 large-offset-1 columns bioHs2">Photoshop</span><span class="small-1 small-offset-9 large-1 large-offset-9 bioHs2 columns">80%</span></span>
           </div>
           <div class="progress large-12 secondary progBar round">
-            <span class="meter progBar3"><span class="large-5 large-offset-1 columns bioHs2">Angular JS</span><span class="large-1 large-offset-5 bioHs2 columns">80%</span></span>
+            <span class="meter progBar3"><span class="small-6 large-5 large-offset-1 columns bioHs2">Angular JS</span><span class="small-1 small-offset-4 large-1 large-offset-5 bioHs2 columns">80%</span></span>
           </div>
           <div class="progress large-12 secondary progBar round">
-            <span class="meter progBar3"><span class="large-1 large-offset-1 columns bioHs2">Firebase</span><span class="large-1 large-offset-9 bioHs2 columns">80%</span></span>
+            <span class="meter progBar3"><span class="small-1 large-1 large-offset-1 columns bioHs2">Firebase</span><span class="small-1 small-offset-9 large-1 large-offset-9 bioHs2 columns">80%</span></span>
           </div>
 
-          <div class="progress large-12 secondary progBar round">
-            <span class="meter progBar1"><span class="large-6 large-offset-1 columns bioHs2">Microsoft Word</span><span class="large-1 large-offset-4 columns bioHs2">90%</span></span>
-          </div>
+
         </div>
       </div>
 
-      <div class="large-1 columns">
+      <div class="small-3 small-centered medium-uncentered medium-2 medium-offset-1 large-uncentered large-1 columns bioImg">
         <img src="img/me.JPG">
       </div>
-      <div class="large-4 large-offset-1 columns">
+      <div class="small-10 small-centered large-uncentered medium-uncentered medium-6 medium-offset-1 large-4 large-offset-1 columns bioImg">
         <h3 class="ps">I am assisting to the Nashville Software School. I enjoy coding and learning new languages. I also do some side jobs like making websites and photography, please contact me for serious inquiries only. Down below are some of my skills and some of my work, enjoy!</h3>
       </div>
     </div>
 
 <!-- What I Offer -->
 
-    <div class="large-12 columns">
-      <div class="large-2 large-centered columns whtIoffer">
-        <h4 class="name3">What I Offer </h4>
-        <div class="large-4 columns"><hr class="hrs2"></div>
+    <div id="offer" class="large-12 columns">
+      <div class="small-12 small-centered medium-4 medium-centered large-2 large-centered columns whtIoffer">
+          <h4 class="name3">WHAT I OFFER </h4>
+        <div class="small-4 small-centered medium-12 large-12 columns"><hr class="hrs2"></div>
       </div>
     </div>
 
     <div class="large-12 columns">
-      <div class="large-3 offset large-offset-1 columns">
-        <img class="sqrs" src="img/responsive1.png">
-      </div>
-      <div class="large-3 offset columns">
-        <img class="sqrs" src="img/clean-code.png">
-      </div>
-      <div class="large-3 offset columns">
-        <img class="sqrs" src="img/design.png">
+      <div class="row">
+        <div class="large-4 large-uncentered sqrsOffset medium-6 medium-centered small-8 columns">
+          <img class="sqrs" src="img/clean-code.png">
+        </div>
+        <div class="large-4 large-uncentered sqrsOffset medium-6 medium-centered small-8 columns">
+          <img class="sqrs" src="img/responsive1.png">
+        </div>
+        <div class=" large-4 large-uncentered sqrsOffset medium-6 medium-centered small-8 columns">
+          <img class="sqrs" src="img/design.png">
+        </div>
       </div>
     </div>
+
+<!--     MY PORTFOLIO -->
+
+    <div id="portfolio" class="large-12 columns">
+      <div class="small-12 medium-4 medium-centered large-2 large-centered columns portfolio">
+        <h4 class="name3">MY PORTFOLIO </h4>
+        <div class="small-4 small-centered medium-12 large-12 columns"><hr class="hrs2"></div>
+      </div>
+    </div>
+
+    <div class="large-12 columns">
+      <div class="row">
+        <div class="small-6 small-centered medium-4 large-4  large-uncentered columns">
+          <img class="imgs" src="img/clearchoice_logo3.png">
+        </div>
+        <div class="small-6 small-centered medium-4 large-5 large-offset-3 large-uncentered columns">
+          <img class="imgs ez" src="img/LOGO%20Jan%202012.png">
+        </div>
+      </div>
+      <div class="row">
+        <div class="small-6 small-centered medium-4 large-4 large-uncentered columns">
+          <img class="imgs imgMargin" src="img/ChefItLOGO.png">
+        </div>
+        <div class="small-6 small-centered medium-4 large-4 large-offset-2 large-uncentered columns">
+          <img class="imgs imgMargin leoroofing" src="img/leoroofing.png">
+        </div>
+      </div>
+    </div>
+
+
+<!-- TESTIMONIALS -->
+    <div id="testimonials" class="large-12 columns testimonials">
+      <div class="large-12 columns">
+        <div class="small-12 medium-4 medium-centered large-2 large-centered columns testName">
+          <h4 class="name4">TESTIMONIALS </h4>
+          <div class="small-4 small-centered medium-12 large-12 columns"><hr class="hrs2"></div>
+        </div>
+      </div>
+      <div class="large-12 columns">
+        <div class="fade">
+          <div>
+            <img class="small-6 small-centered large-3 large-centered columns" src="img/clearchoice_logo3.png">
+            <p class="small-6 small-centered large-5 large-centered columns bioHs1 slideP">Thanks Brandon for the great job you did for our companies website. It's exactly what we wanted!</p>
+          </div>
+          <div>
+            <img class="small-6 small-centered large-3 large-centered columns" src="img/leoroofing.png">
+            <p class="small-6 small-centered large-5 large-centered columns bioHs1 slideP">Great job! Amazing work you did for our company in creating our website!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
 
 <!--     CONTACT FORM -->
 
@@ -120,7 +196,7 @@
 
 				 ?>
 
-    <div id="contact" class="large-12 columns contactForm">
+    <div class=" small-10 small-centered medium-10 medium-centered large-12 columns ">
     	<div class="large-10 large-centered columns forma">
 
 
@@ -130,8 +206,8 @@
 
 
         <div class="section-container tabs large-10 large-centered columns" data-section>
-          <section class="section">
-            <div class="large-3 large-centered columns"><h2 class="contactH1"><a href="#panel1" style="color:#ff4a69;">Contact Me</a></h2></div>
+          <section id="cont" class="section">
+            <div class="small-7 small-centered medium-4 medium-centered large-3 large-centered columns"><h2 class="contactH1"><a class="contacth1" href="#panel1" style="color:#cfba65;">Contact Me</a></h2></div>
             <div class="content" data-slug="panel1">
 
 
@@ -173,7 +249,7 @@
                 <!-- START : COMMENTS SECTION -->
 
                 <div class="large-7 large-centered columns"><textarea rows="4" name="Comments" placeholder="Your Message" style="background: none;"><?php get_data("Comments"); ?></textarea>
-                <button style="background: #ff4a69;" type="submit" class="radius button">Submit</button></div>
+                <button style="background: #000; color: #cfba65; font-family: 'Julius Sans One', sans-serif;" type="submit" class="radius button">Submit</button></div>
 
               </form>
 
